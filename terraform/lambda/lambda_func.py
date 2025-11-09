645da199fd4566cca7aaf03359036d7ee90e1aea
Vasyl-Poremchuk <nleveryone@gmail.com>
2025-11-09 17:43:37 +0200

    Add a Lambda trigger function

---
+++ terraform/lambda/lambda_func.py
@@ -0,0 +1,62 @@
+import json
+import os
+
+import boto3
+from structlog import get_logger
+
+
+logger = get_logger(__name__)
+ecs_client = boto3.client("ecs")
+
+
+def trigger_ecs_task_handler(event, context) -> dict:
+    """Lambda function handler to trigger book data scraping
+    & parsing as an ECS task.
+
+    :param event: Lambda event data.
+    :param context: Lambda context object.
+    :return: Response containing task status information.
+    """
+    cluster_name = os.environ.get("CLUSTER_NAME")
+    task_definition = os.environ.get("TASK_DEFINITION")
+    subnet_id = os.environ.get("SUBNET_ID")
+    security_group_id = os.environ.get("SECURITY_GROUP_ID")
+
+    params = {
+        "cluster": cluster_name,
+        "taskDefinition": task_definition,
+        "count": 1,
+        "launchType": "FARGATE",
+        "networkConfiguration": {
+            "awsvpcConfiguration": {
+                "subnets": [subnet_id],
+                "securityGroups": [security_group_id],
+                "assignPublicIp": "ENABLED",
+            }
+        },
+    }
+
+    try:
+        response = ecs_client.run_task(**params)
+        logger.info(f"ECS task started: {json.dumps(response, default=str)}")
+
+        task_arn = (
+            response.get("tasks")[0].get("taskArn")
+            if response.get("tasks")
+            else None
+        )
+
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(
+                {
+                    "message": "Book data scraping & parsing task "
+                    "started successfully",
+                    "taskArn": task_arn,
+                }
+            ),
+        }
+
+        return response
+    except Exception as exc:
+        raise exc
